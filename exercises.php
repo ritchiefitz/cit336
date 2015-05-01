70ef599463c0e29bd87817045d39b0dcd02602d9
Ritchie Fitzgerald <ritchiefitz1@gmail.com>
2015-05-01 14:23:45 -0600

    First Week Push

--- exercises.php
+++ exercises.php
@@ -4,11 +4,33 @@
 	<h1 class="page-title">Exercises</h1>
 	<h2>Unit 2</h2>
 	<div class="week">
-		<h3>Week 1</h3>
+		<h3>Exercises</h3>
 		<ul>
-			<li><a href="units/unit_2/exercise_1_1.php">Exercise 1-1</a></li>
-			<li><a href="units/unit_2/exercise_1_2.php">Exercise 1-2</a></li>
-			<li><a href="units/unit_2/exercise_1_3.php">Exercise 1-3</a></li>
+			<li><a href="#">Exercise 1-1</a> I did it.</li>
+			<li><a href="#">Exercise 1-2</a> I did it.</li>
+			<li><a href="#">Exercise 1-3</a> I did it.</li>
+			<li><a href="#">Exercise 2-1</a></li>
+			<li><a href="#">Exercise 3-1</a></li>
+			<li><a href="#">Exercise 4-1</a></li>
+			<li><a href="#">Exercise 5-1</a></li>
+			<li><a href="#">Exercise 6-1</a></li>
+			<li><a href="#">Exercise 6-2</a></li>
+			<li><a href="#">Exercise 7-2</a></li>
+			<li><a href="#">Exercise 8-2</a></li>
+			<li><a href="#">Exercise 11-1</a></li>
+			<li><a href="#">Exercise 13-1</a></li>
+			<li><a href="#">Exercise 12-1</a></li>
+			<li><a href="#">Exercise 15-1</a></li>
+		</ul>
+		<h3>Enhancements</h3>
+		<ul>
+			<li><a href="#">Enhancement 2-1</a></li>
+			<li><a href="#">Enhancement 4-1</a></li>
+			<li><a href="#">Enhancement 5-1</a></li>
+			<li><a href="#">Enhancement 7-2</a></li>
+			<li><a href="#">Enhancement 11-1</a></li>
+			<li><a href="#">Enhancement 13-1</a></li>
+			<li><a href="#">Enhancement 12-1</a></li>
 		</ul>
 	</div>
 </article>
